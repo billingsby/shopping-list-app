79b5fb5d5f4c55fb32c9947176232e0dccc85c23
Billingsby <mark@markbillingsby.com>
2016-02-14 09:13:40 -0700

    functionality working

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 //Adds Item to List
 $('.add-item').keyup(function (e) {
   if (e.which == 13) {
-    $('.item-list').prepend('<li class=\"list-item\"> <input type=\"checkbox\" value=\"None\" id=\"' + $('.add-item').val() + '\" name=\"check\"/> <label for=\"' + $('.add-item').val() + '\"></label><h3 class=\"item\">' +
+    $('.item-list').prepend('<li class=\"list-item\"> <input class=\"checkbox\" type=\"checkbox\" value=\"None\" id=\"' + $('.add-item').val() + '\" name=\"check\"/> <label for=\"' + $('.add-item').val() + '\"></label><h3 class=\"item\">' +
     $('.add-item').val() + '</h3></li>');
     
     $('.add-item').val('+ Add an item...');
@@ -18,19 +18,21 @@ $('.add-item').keyup(function (e) {
 
 // Checked items move to checked list
 
-// $('input[type="checkbox"]').click(function() {
-  
-//   $('.list-item').remove();
-//   $('.checked-list').prepend('.list-item');
-//   $('.list-item').slideUp();
-// });
-
-
-  $('.list-item input[type="checkbox"]').click(function() {
-    
+$('.item-list').on('click', '.checkbox', function() {
+    console.log('clicked');
     $(this).
-      prop('checked', true).
+      attr('checked','checked').
       closest('.list-item').removeClass('list-item').addClass('list-item-checked').prependTo($('.checked-list'));
+  
+});
+
+// Move items back to item list
+
+$('.checked-list').on('click', '.checkbox', function() {
+    console.log('clicked');
+    $(this).
+      removeAttr('checked').
+      closest('.list-item-checked').removeClass('list-item-checked').addClass('list-item').appendTo($('.item-list'));
   
 });
   
@@ -45,7 +47,7 @@ $('.show-list-style').click(function() {
 // });
 
 
-
+// .removeAttr('checked')
 
 
 })();
